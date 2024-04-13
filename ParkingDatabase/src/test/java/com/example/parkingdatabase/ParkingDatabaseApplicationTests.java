cb70d353a2109b8ca6508687cb2d4fedb8e99f1e
Naitsyrk95 <kry.zawadzki2203@gmail.com>
2024-04-13 17:35:32 +0200

    Add files via upload

--- ParkingDatabase/src/test/java/com/example/parkingdatabase/ParkingDatabaseApplicationTests.java
+++ ParkingDatabase/src/test/java/com/example/parkingdatabase/ParkingDatabaseApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.parkingdatabase;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,19 @@ class ParkingDatabaseApplicationTests {
 
     @Test
     void contextLoads() {
+
+
+
+
+
     }
+
+
+    @Test
+    @DisplayName("Should make a list with all cars on the parking")
+
+
+    ParkingDatabaseApplicationTests test = new ParkingDatabaseApplicationTests();
+
 
 }
