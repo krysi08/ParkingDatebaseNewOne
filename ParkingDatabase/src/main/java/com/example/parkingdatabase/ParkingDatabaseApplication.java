831807fc4bb495d402747fc54cff42b836051028
krysi08 <145769907+krysi08@users.noreply.github.com>
2024-04-13 15:14:00 +0200

    Add files via upload

---
+++ ParkingDatabase/src/main/java/com/example/parkingdatabase/ParkingDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkingdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingDatabaseApplication.class, args);
+    }
+
+}
---
+++ ParkingDatabase/src/main/java/com/example/parkingdatabase/Run.java
@@ -0,0 +1,41 @@
+package com.example.parkingdatabase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Run {
+
+    private CarRepo carRepo;
+    @Autowired
+    public Run(CarRepo carRepo) {
+        this.carRepo = carRepo;
+    }
+
+
+
+    public void addNewCar() {
+
+        Car car = new Car("ABC23409","Opel","Astra",Color.BLACK);
+        Car car2 = new Car("YFH23409", "Renault", "5W", Color.RED);
+        Car car3 = new Car("OFC23409", "Peugeot", "206", Color.WHITE);
+
+        carRepo.save(car);
+        carRepo.save(car2);
+        carRepo.save(car3);
+
+
+
+    Iterable<Car> all = carRepo.findAll();
+    all.forEach(System.out::println);
+    }
+
+    public void removeCar() {
+        Car car2 = new Car("ABC23409", "Opel", "Astra", Color.BLACK);
+        carRepo.delete(car2);
+
+        Iterable<Car> all = carRepo.findAll();
+        all.forEach(System.out::println);
+    }
+
+}
